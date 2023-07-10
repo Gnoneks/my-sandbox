24ce319a0297b9cf36da77685c1cd38b7e4456d2
Marcin Żuk <Gnoneks@gmail.com>
2023-07-10 12:21:17 +0200

    Added components & router paths

---
+++ src/app/screens/invalid-link-screen/invalid-link-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invalid-link-screen',
+  templateUrl: './invalid-link-screen.component.html',
+  styleUrls: ['./invalid-link-screen.component.scss']
+})
+export class InvalidLinkScreenComponent {
+
+}
